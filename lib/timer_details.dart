de5e89c03ec97f6a952d0951b7ecb32def3cec2d
Auther <Auther@gmail.com>
2024-01-10 17:21:33 +0530

    update deisgn

---
+++ lib/timer_details.dart
@@ -0,0 +1,523 @@
+import 'package:flutter/material.dart';
+
+class TimerDetailsPage extends StatefulWidget {
+  const TimerDetailsPage({super.key});
+
+  @override
+  State<TimerDetailsPage> createState() => _TimerDetailsPageState();
+}
+
+class _TimerDetailsPageState extends State<TimerDetailsPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = TabController(length: 2, vsync: this);
+
+    _controller.addListener(() {
+      setState(() {});
+      print("Selected Index: " + _controller.index.toString());
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.antiAlias,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment(-0.16, -0.99),
+          end: Alignment(0.16, 0.99),
+          colors: [Color(0xFF0C1D4D), Color(0xFF214ECC)],
+        ),
+      ),
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+              bottom: TabBar(controller: _controller, tabs: const [
+                Tab(
+                  text: "TimeSheet",
+                ),
+                Tab(
+                  text: "Details",
+                )
+              ]),
+              title: const Text("Get to know Apexer - Ivan"),
+              actions: [
+                IconButton(onPressed: () {}, icon: const Icon(Icons.edit))
+              ]),
+          body: TabBarView(
+              controller: _controller,
+              children: const [TimeSheetTab(), detailsTab()]),
+        ),
+      ),
+    );
+  }
+}
+
+class detailsTab extends StatelessWidget {
+  const detailsTab({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: double.infinity,
+          margin: const EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.07999999821186066),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Project',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0.40,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Row(
+                children: [
+                  Container(
+                    width: 2,
+                    height: 24,
+                    decoration: ShapeDecoration(
+                      color: Color(0xFFFFC629),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4)),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  const Text(
+                    'Apexive: Content Planning',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 0.15,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Deadline ',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0.40,
+                ),
+              ),
+              SizedBox(height: 4),
+              Text(
+                '10/11/2023',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0.15,
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Assigned to',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0.40,
+                ),
+              ),
+              SizedBox(height: 4),
+              Text(
+                'Ivan Zhuikov',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0.15,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          height: 136,
+          margin: const EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.07999999821186066),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Description',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w400,
+                  // height: 0.10,
+                  letterSpacing: 0.25,
+                ),
+              ),
+              SizedBox(height: 4),
+              Text(
+                'As a user, I would like to be able to buy a subscription, this would allow me to get a discount on the products and on the second stage of diagnosis',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w500,
+                  // height: 0.10,
+                  letterSpacing: 0.10,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TimeSheetTab extends StatelessWidget {
+  const TimeSheetTab({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      // crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: const EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.07999999821186066),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //
+              const Text(
+                'Monday ',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0.40,
+                ),
+              ),
+              const SizedBox(height: 4),
+              const Text(
+                '10/11/2023',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0.15,
+                ),
+              ),
+              const SizedBox(height: 4),
+              const Text(
+                'Start Time 10:00 ',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0.40,
+                ),
+              ),
+
+              Row(
+                children: [
+                  const Text(
+                    '08:08:20',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 36,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.stop_circle,
+                        size: 48,
+                      )),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.play_circle,
+                        size: 48,
+                      ))
+                ],
+              ),
+
+              const SizedBox(height: 16),
+              const Divider(
+                color: Colors.white,
+              ),
+              const SizedBox(height: 16),
+
+              const Text(
+                'Description',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w400,
+                  // height: 0.10,
+                  letterSpacing: 0.25,
+                ),
+              ),
+              SizedBox(height: 4),
+              const Text(
+                'As a user, I would like to be able to buy a subscription, this would allow me to get a discount on the products and on the second stage of diagnosis',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w500,
+                  // height: 0.10,
+                  letterSpacing: 0.10,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(left: 20.0),
+          child: Text(
+            'Completed Records',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 12,
+              fontFamily: 'Inter',
+              fontWeight: FontWeight.w400,
+              letterSpacing: 0.40,
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.07999999821186066),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Monday ',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0.40,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      const Text(
+                        '16.06.2023',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 0.15,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      const Text(
+                        'Start Time 10:00 ',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0.40,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                      height: 32,
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      decoration: ShapeDecoration(
+                        color: Colors.white.withOpacity(0.1599999964237213),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(64),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          '08:00',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 0.10,
+                          ),
+                        ),
+                      ))
+                ],
+              ),
+            ],
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.07999999821186066),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Monday ',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0.40,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      const Text(
+                        '16.06.2023',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 0.15,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      const Text(
+                        'Start Time 10:00 ',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0.40,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                      height: 32,
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      decoration: ShapeDecoration(
+                        color: Colors.white.withOpacity(0.1599999964237213),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(64),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          '08:00',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 0.10,
+                          ),
+                        ),
+                      ))
+                ],
+              ),
+              const SizedBox(height: 16),
+              const Divider(
+                color: Colors.white,
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Description',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w400,
+                  // height: 0.10,
+                  letterSpacing: 0.25,
+                ),
+              ),
+              SizedBox(height: 4),
+              const Text(
+                'As a user, I would like to be able to buy a subscription, this would allow me to get a discount on the products and on the second stage of diagnosis',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w500,
+                  // height: 0.10,
+                  letterSpacing: 0.10,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
