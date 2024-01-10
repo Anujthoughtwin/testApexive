07301bb06e2dafbc7f7f28a77a1a850d4f38a613
aniket <anuj@thoughtwin.com>
2024-01-10 15:20:09 +0530

    Commit to merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'create_timer.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,11 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CreateTimer(),
     );
   }
 }
